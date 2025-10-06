45f610991843e45d262b9fe1ff78fc85cad895a4
Diego <diego.sa@pricefy.com.br>
2025-10-06 02:06:38 -0300

    removendo trecho pra poder usar o cognito

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -11,10 +11,10 @@ resource "aws_lambda_function" "auth" {
   timeout         = 30
   memory_size     = 512
 
-  vpc_config {
-    subnet_ids         = data.aws_subnets.supported.ids
-    security_group_ids = [local.shared_sg_id]
-  }
+  #vpc_config {
+  #  subnet_ids         = data.aws_subnets.supported.ids
+  #  security_group_ids = [local.shared_sg_id]
+  #}
 
   environment {
     variables = {
